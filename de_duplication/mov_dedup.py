3c841e804bcc820e8f4531beaa19fdb68670f13a
ebiiim <mail@ebiiim.com>
2018-08-19 00:28:27 +0900

    run.py -> single_dedup.py

---
+++ de_duplication/mov_dedup.py
@@ -0,0 +1,117 @@
+import os
+import sys
+import shutil
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import datetime
+from logging import getLogger
+logger = getLogger(__name__)
+
+
+class MovDeDup(object):
+
+    def __init__(self, path_tmp='/tmp', path_input='/input', path_output='/output',
+                 path_bin='/bin', path_ffmpeg='/ffmpeg.exe'):
+        # self.path_self = os.path.dirname(os.path.abspath(__file__))
+        self.path_self = '.'
+        self.path_tmp = self.path_self + path_tmp
+        self.dir_input = path_input
+        self.path_input = self.path_self + self.dir_input
+        self.path_output = self.path_self + path_output
+        self.path_bin = self.path_self + path_bin
+        self.path_ffmpeg = self.path_bin + path_ffmpeg
+        self._directories = [self.path_tmp, self.path_input, self.path_output, self.path_bin, ]
+        self._binaries = [self.path_ffmpeg, ]
+
+    def init_check(self, ):
+        logger.debug('check dirs')
+        for path in self._directories:
+            if not os.path.exists(path):
+                logger.debug('mkdir: ' + path)
+                os.makedirs(path)
+
+        logger.debug('check bins')
+        for path in self._binaries:
+            if not os.path.exists(path):
+                logger.critical('not found:' + path)
+                sys.exit(1)
+
+    def ssim_hist_gen(self, df: pd.DataFrame, target_col_name, bins=1000, min_ssim=0.90, max_ssim=1.0, cumulative=True):
+        fig = plt.figure()
+        ax = fig.add_subplot(1, 1, 1)
+        df[target_col_name].hist(ax=ax, bins=bins, range=[min_ssim, max_ssim], cumulative=cumulative)
+        ax.set_xlabel('SSIM')
+        ax.set_ylabel('Freq.')
+        ax.xaxis.set_major_formatter(plt.FormatStrFormatter('%.4g'))
+        # ax.set_title('Similarity')
+        # plt.plot()
+        # fig.show()
+        plt.savefig(self.path_output + '/' + datetime.datetime.now().strftime('%Y%m%d%H%M%SZ') + '.png')
+
+    @staticmethod
+    def load_similarity_csv(similarity_csv) -> pd.DataFrame:
+        df = pd.read_csv(similarity_csv, index_col=0)
+        logger.debug(df.head(3))
+        logger.debug(df.info())
+        logger.debug(df.describe())
+        return df
+
+    @staticmethod
+    def get_copy_lists(df: pd.DataFrame, target_col_name, ssim_threshold):
+        """
+        (src_list, del_list)を返す。
+        del_listはSSIMしきい値以上のファイル名を格納したリスト。
+        src_listはdel_listの1個前のファイル名を格納したリスト。
+        利用方法: del_listのファイルを削除して、src_listのファイルをdel_listの同indexのファイル名で保存する。
+        """
+        col_filename = 'FileName'
+
+        # 重複フレームのindexを取得
+        copy_index_list = df[df[target_col_name] >= ssim_threshold].index
+
+        # 重複率の表示
+        logger.info('ssim_threshold: ' + str(ssim_threshold))
+        lo = len(df)
+        lc = len(copy_index_list)
+        logger.info('length_original: ' + str(lo))
+        logger.info('length_copy: ' + str(lc))
+        logger.info('rate: ' + str(lc/lo))
+
+        # コピー元ファイル名リストとコピー先ファイル名リストを作る
+        src_list = list()
+        del_list = list()
+        for each in copy_index_list:
+            # コピー元ファイル名リスト
+            for n in range(each - 1):  # range(each)だと1枚目まで見に行く、2枚目開始なのでrange(each - 1)
+                # n枚前が重複でない場合は採用（重複なら飛ばす）
+                if list(df.query('index == ' + str(each - n))[target_col_name])[0] < ssim_threshold:
+                    src_list.extend(list(df.query('index == ' + str(each - n))[col_filename]))
+                    break
+            # コピー先ファイル名リスト
+            del_list.extend(list(df.query('index == ' + str(each))[col_filename]))
+
+        return list(del_list), list(src_list)
+
+    def copy_dedup(self, del_list):
+        """
+        input/のファイルをdel_listを除きすべてindex/からtmp/にコピーする。
+        """
+        input_list = ['.' + self.dir_input + '/' + each for each in os.listdir(self.path_input)]
+        dedup_list = list(set(input_list) - set(del_list))
+        print(dedup_list)
+        for file in dedup_list:
+            copy_src = file
+            copy_dst = self.path_tmp + '/' + file.split('/')[-1]
+            shutil.copy2(copy_src, copy_dst)
+            logger.debug('copy: ' + copy_src + ' -> ' + copy_dst)
+
+    def copy_dup(self, dup_list, src_list):
+        """
+        input/のsrc_listに記載のファイルをdup_listに記載の名前でtmp/からtmp/にコピーする。
+        """
+        for (dup, src) in zip(dup_list, src_list):
+            copy_src = self.path_tmp + '/' + src.split('/')[-1]
+            copy_dst = self.path_tmp + '/' + dup.split('/')[-1]
+            shutil.copy2(copy_src, copy_dst)
+            logger.debug('copy: ' + copy_src + ' -> ' + copy_dst)
--- readme.md
+++ readme.md
@@ -1,6 +1,6 @@
 # mov-dedup
 
-動画から同一フレームを取り除くやつ（プロトタイプ）
+動画から同一フレームを取り除くツール
 
 ## Preparation
 
@@ -8,19 +8,22 @@
 2. `pip install -r requirements.txt`
 3. `/bin/`に`ffmpeg.exe`を置く（4.0.0確認済） [FFmpeg](https://ffmpeg.zeranoe.com/builds/)
 
-## Usage
+## Mode1: Single (single_dedup.py)
+### Usage
 
 5種類のコマンド `hist` `check` `copy1` `copy2` `enc`
 
 Examples
 
-- `run.py hist ./similarity.csv`: SSIM 0.90--1.00 の積み上げのヒストグラムを`output/`に出力する
-- `run.py check 0.85 ./similarity.csv`: `similarity.csv`の`SSIM(Whole)`で`0.85`以上の枚数や比率をコンソールに出力する
-- `run.py copy1 0.85 ./similarity.csv`: `input/`のうち重複しないフレーム（SSIM < `0.85`）だけを`tmp/`にコピーする
-- `run.py copy2 0.85 ./similarity.csv`: `input/`のうち重複するフレーム（SSIM > `0.85`）だけ、前の重複しないフレームを __`tmp/` から `tmp/` に__ コピーする
-- `run.py enc ./similarity.csv`: `tmp/`の中身をffmpegで動画に変換して`output/`に出力する（`output/audio.aac`を事前に置くこと）
+- `single_dedup.py hist ./similarity.csv`: SSIM 0.90--1.00 の積み上げのヒストグラムを`output/`に出力する
+- `single_dedup.py check 0.85 ./similarity.csv`: `similarity.csv`の`SSIM(Whole)`で`0.85`以上の枚数や比率をコンソールに出力する
+- `single_dedup.py copy1 0.85 ./similarity.csv`: `input/`のうち重複しないフレーム（SSIM < `0.85`）だけを`tmp/`にコピーする
+- `single_dedup.py copy2 0.85 ./similarity.csv`: `input/`のうち重複するフレーム（SSIM > `0.85`）だけ、前の重複しないフレームを __`tmp/` から `tmp/` に__ コピーする
+- `single_dedup.py enc ./similarity.csv`: `tmp/`の中身をffmpegで動画に変換して`output/`に出力する（`output/audio.aac`を事前に置くこと）
 
-## Workflow
+`enc`はおまけ程度の機能しかない。`enc`では`./similarity.csv`は使わない（引数が2個あれば良い）。
+
+### Workflow
 
 1. 準備
     1. ffmpegで動画を画像と音声に分離する
@@ -35,7 +38,3 @@ Examples
     1. `copy2`で重複する画像を`tmp/`内でコピーする
 1. エンコード
     1. `enc`で`tmp/`内を画像と`output/audio.aac`を動画に変換して`output/`に出力する
-
-### TODO
-
-いろんなオプションに対応したい...
