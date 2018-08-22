6416965c0d14e4dd529cf8b35f7ba02eb052e5f0
ebiiim <mail@ebiiim.com>
2018-08-23 00:34:06 +0900

    Pillow -> OpenCV

--- divide_conquer/divider.py
+++ divide_conquer/divider.py
@@ -1,5 +1,5 @@
 import os
-from PIL import Image
+import cv2
 from logging import getLogger
 logger = getLogger(__name__)
 
@@ -26,22 +26,27 @@ class MovDivider(object):
 
     @staticmethod
     def divide_image(src, dst_dirs, divide_w, divide_h):
-        im = Image.open(src)
-        im_w = im.size[0]
-        im_h = im.size[1]
+        im = cv2.imread(src)
+        im_w = im.shape[1]
+        im_h = im.shape[0]
         crop_w = im_w/divide_w
         crop_h = im_h/divide_h
         logger.debug('(src, w, h, crop) = ('+src+', '+str(im_w)+', '+str(im_h)+', '+str(divide_w*divide_h)+')')
 
+        # 切り取りリストを作成する: (box, save_path)のlist
+        divide_list = list()
         file_name = src.split('/')[-1]
         idx_dir = 0
         for idx_h in range(divide_h):
             for idx_w in range(divide_w):
-                box = (crop_w*idx_w, crop_h*idx_h, crop_w*(idx_w+1), crop_h*(idx_h+1))
+                box = (int(crop_w*idx_w), int(crop_h*idx_h), int(crop_w*(idx_w+1)), int(crop_h*(idx_h+1)))
                 # logger.debug(str(idx_dir) + ': ' + str(box))
-                tmp_im = im.crop(box)
-                tmp_im.save(dst_dirs[idx_dir] + '/' + file_name)
+                save_path = dst_dirs[idx_dir] + '/' + file_name
+                divide_list.append((box, save_path))
                 idx_dir = idx_dir+1
+
+        # 切り取りリストに従って処理
+        return [cv2.imwrite(s, im[b[1]:b[3], b[0]:b[2]]) for b, s in divide_list]
 
     @staticmethod
     def devide_images(src_dir, divide_w, divide_h):
