a064fb4a6409ad7c95bb350900d5d6b88451adaa
ebiiim <mail@ebiiim.com>
2018-09-16 23:12:26 +0900

    update run.py

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+import os
 import time
 from pathlib import Path
 import shutil
@@ -12,7 +13,11 @@ logger = getLogger(__name__)
 
 
 def run_all(input_file='input-movie.mp4', output_file='output-movie.mp4',
-            threshold=55, divide_w=4, divide_h=3, cleanup = True):
+            threshold=45, divide_w=8, divide_h=6,
+            waifu2x_scale_ratio=2, waifu2x_crop_size=128,
+            ffmpeg_preset='fast', ffmpeg_crf=22,
+            ffmpeg_resize=False, ffmpeg_resize_w=2560, ffmpeg_resize_h=1440,
+            cleanup=True):
     logger.info('Input/Output: '+input_file+' -> '+output_file)
     logger.info('PSNR Threshold: '+str(threshold))
     logger.info('divide(W, H): ('+str(divide_w)+', '+str(divide_h)+')')
@@ -27,29 +32,37 @@ def run_all(input_file='input-movie.mp4', output_file='output-movie.mp4',
     tmp_dir = MovieConverter.mov2pic(input_file, 'tmp-input')
     logger.info('divide images')
     divided_dirs = MovDivider.divide_images(tmp_dir, divide_w, divide_h)
+    if cleanup:
+        shutil.rmtree(tmp_dir)
     logger.info('calc PSNR')
     for idx, each in enumerate(divided_dirs):
         CalcPSNR.calc_psnr_frames(each, Path(each).parent.joinpath('psnr_' + '{:04g}'.format(idx + 1) + '.csv'))
     logger.info('copy unique images')
     copied_dirs = MultiDeDup.multi_copy1(divided_dirs, threshold)
+    if cleanup:
+        shutil.rmtree(Path(divided_dirs[0]).resolve().parent)
     logger.info('waifu2x')
     for each in copied_dirs:
-        Waifu2xUpscaler.upscale_dir(each, each)
+        Waifu2xUpscaler.upscale_dir(each, each, scale_ratio=waifu2x_scale_ratio, crop_size=waifu2x_crop_size)
     logger.info('copy duplicated images')
     MultiDeDup.multi_copy2(divided_dirs, threshold)
     logger.info('combine images')
     copied_dir = Path(copied_dirs[0]).resolve().parent
     combined_dir = MovCombiner.combine_images(copied_dir, divide_w, divide_h)
+    if cleanup:
+        shutil.rmtree(copied_dir)
     logger.info('convert images to video')
-    p2m_out = MovieConverter.pic2mov(input_dir=combined_dir, output_file='tmp-movie.mp4', fps=fps, preset='fast')
+    p2m_out = MovieConverter.pic2mov(input_dir=combined_dir, output_file='tmp-movie.mp4', fps=fps,
+                                     preset=ffmpeg_preset, crf=ffmpeg_crf,
+                                     resize=ffmpeg_resize, resize_w=ffmpeg_resize_w, resize_h=ffmpeg_resize_h)
+    if cleanup:
+        shutil.rmtree(combined_dir)
     logger.info('mix audio and video')
     MovieConverter.mix(p2m_out, sound_file, output_file)
     if cleanup:
-        logger.info('cleanup')
-        for each in [sound_file, tmp_dir, Path(divided_dirs[0]).resolve().parent, copied_dir, p2m_out]:
-            shutil.rmtree(each)
-    logger.info('completed')
+        [os.remove(f) for f in [sound_file, p2m_out, ]]
 
+    logger.info('completed')
     elapsed_time = time.time() - start_time
     logger.info(time.strftime('elapsed time: %H:%M:%S', time.gmtime(elapsed_time)))
 
@@ -60,4 +73,4 @@ if __name__ == '__main__':
     LOG_LEVEL = logging.INFO
     logging.basicConfig(format=LOG_FMT, level=LOG_LEVEL)
 
-    run_all(cleanup=False)
+    run_all()
