4e29a7ff2e385cdb7b4ce0e915021b01055beb71
ebiiim <mail@ebiiim.com>
2018-09-16 23:16:41 +0900

    delete tmp_dirs (unused)

--- de_duplication/mdedup.py
+++ de_duplication/mdedup.py
@@ -12,26 +12,24 @@ class MultiDeDup(object):
         similarity_files = [src_dirs_base.joinpath('psnr_' + '{:04g}'.format(i + 1) + '.csv').as_posix()
                             for i in range(len(src_dirs))]
         threshold_values = [threshold, ] * len(src_dirs)
-        tmp_dirs = [src_dir.replace('input', 'tmp') for src_dir in src_dirs]
         dst_dirs = [src_dir.replace('input', 'output') for src_dir in src_dirs]
 
         logger.info('src_dirs: ' + str(src_dirs))
-        logger.info('tmp_dirs: ' + str(tmp_dirs))
         logger.info('dst_dirs: ' + str(dst_dirs))
         logger.info('similarity_files: ' + str(similarity_files))
         logger.info('threshold_values: ' + str(threshold_values))
 
-        for path in [Path(tmp_dirs[0]).parent, Path(dst_dirs[0]).parent]:
-            if not path.exists():
-                logger.debug('mkdir: ' + path.as_posix())
-                path.mkdir()
+        mk_path = Path(dst_dirs[0]).parent
+        if not mk_path.exists():
+            logger.debug('mkdir: ' + mk_path.as_posix())
+            mk_path.mkdir()
 
-        return src_dirs, tmp_dirs, dst_dirs, similarity_files, threshold_values
+        return src_dirs, dst_dirs, similarity_files, threshold_values
 
     @staticmethod
     def multi_copy1(src_dirs, threshold):
-        src_dirs, tmp_dirs, dst_dirs, similarity_files, threshold_values = MultiDeDup.get_paths(src_dirs, threshold)
-        for src, tmp, dst, sf, tv in zip(src_dirs, tmp_dirs, dst_dirs, similarity_files, threshold_values):
+        src_dirs, dst_dirs, similarity_files, threshold_values = MultiDeDup.get_paths(src_dirs, threshold)
+        for src, dst, sf, tv in zip(src_dirs, dst_dirs, similarity_files, threshold_values):
             mdp = MovDeDup(path_input=src, path_tmp=dst, path_output=dst)
             mdp.init_check()
 
@@ -42,8 +40,8 @@ class MultiDeDup(object):
 
     @staticmethod
     def multi_copy2(src_dirs, threshold):
-        src_dirs, tmp_dirs, dst_dirs, similarity_files, threshold_values = MultiDeDup.get_paths(src_dirs, threshold)
-        for src, tmp, dst, sf, tv in zip(src_dirs, tmp_dirs, dst_dirs, similarity_files, threshold_values):
+        src_dirs, dst_dirs, similarity_files, threshold_values = MultiDeDup.get_paths(src_dirs, threshold)
+        for src, dst, sf, tv in zip(src_dirs, dst_dirs, similarity_files, threshold_values):
             mdp = MovDeDup(path_input=src, path_tmp=dst, path_output=dst)
             mdp.init_check()
 
