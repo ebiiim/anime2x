b16f96f4938b4fabada34a100235840b8d987762
ebiiim <mail@ebiiim.com>
2018-09-17 10:07:01 +0900

    fix run.py

--- run.py
+++ run.py
@@ -13,7 +13,7 @@ logger = getLogger(__name__)
 
 
 def run_all(input_file='input-movie.mp4', output_file='output-movie.mp4',
-            threshold=45, divide_w=8, divide_h=6,
+            threshold=55, divide_w=8, divide_h=6,
             waifu2x_scale_ratio=2, waifu2x_crop_size=128,
             ffmpeg_preset='fast', ffmpeg_crf=22,
             ffmpeg_resize=False, ffmpeg_resize_w=2560, ffmpeg_resize_h=1440,
@@ -32,15 +32,11 @@ def run_all(input_file='input-movie.mp4', output_file='output-movie.mp4',
     tmp_dir = MovieConverter.mov2pic(input_file, 'tmp-input')
     logger.info('divide images')
     divided_dirs = MovDivider.divide_images(tmp_dir, divide_w, divide_h)
-    if cleanup:
-        shutil.rmtree(tmp_dir)
     logger.info('calc PSNR')
     for idx, each in enumerate(divided_dirs):
         CalcPSNR.calc_psnr_frames(each, Path(each).parent.joinpath('psnr_' + '{:04g}'.format(idx + 1) + '.csv'))
     logger.info('copy unique images')
     copied_dirs = MultiDeDup.multi_copy1(divided_dirs, threshold)
-    if cleanup:
-        shutil.rmtree(Path(divided_dirs[0]).resolve().parent)
     logger.info('waifu2x')
     for each in copied_dirs:
         Waifu2xUpscaler.upscale_dir(each, each, scale_ratio=waifu2x_scale_ratio, crop_size=waifu2x_crop_size)
@@ -49,18 +45,15 @@ def run_all(input_file='input-movie.mp4', output_file='output-movie.mp4',
     logger.info('combine images')
     copied_dir = Path(copied_dirs[0]).resolve().parent
     combined_dir = MovCombiner.combine_images(copied_dir, divide_w, divide_h)
-    if cleanup:
-        shutil.rmtree(copied_dir)
     logger.info('convert images to video')
     p2m_out = MovieConverter.pic2mov(input_dir=combined_dir, output_file='tmp-movie.mp4', fps=fps,
                                      preset=ffmpeg_preset, crf=ffmpeg_crf,
                                      resize=ffmpeg_resize, resize_w=ffmpeg_resize_w, resize_h=ffmpeg_resize_h)
-    if cleanup:
-        shutil.rmtree(combined_dir)
     logger.info('mix audio and video')
     MovieConverter.mix(p2m_out, sound_file, output_file)
     if cleanup:
         [os.remove(f) for f in [sound_file, p2m_out, ]]
+        [shutil.rmtree(d) for d in [tmp_dir, Path(divided_dirs[0]).resolve().parent, copied_dir, combined_dir]]
 
     logger.info('completed')
     elapsed_time = time.time() - start_time
